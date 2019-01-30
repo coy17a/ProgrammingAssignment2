dd597a4689be14ae1034d15c4c595ce492fbfb11
Alejo <alejocoy17a@gmail.com>
2019-01-30 15:00:01 -0700

    Solution assigment commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## Programing Assigment 2 Coursera R Prograing course
+##By Alejandro Coy Jan 30/2019
 
-## Write a short comment describing this function
+## This function create a matrix object with set,get,setinv, and getinv functions
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) inverse <<- inv
+  getinv <- function() inverse
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  }
 
-}
-
-
-## Write a short comment describing this function
+## This function returns the invers of matrix assuing is a square invertible matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinv()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data)
+  x$setinv(inverse)
+  inverse
 }
